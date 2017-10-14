37af0ae445162b1536b0b8e1ed515bde89881ca3
Robert Kuraj <kuraj.robert@gmail.com>
2017-10-14 20:58:12 +0200

    Implement custom control, add starimages to assets

--- foodTracker/ratingControl.swift
+++ foodTracker/ratingControl.swift
@@ -17,5 +17,64 @@ class ratingControl: UIStackView {
         // Drawing code
     }
     */
-
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    @IBInspectable var starSize: CGSize = CGSize(width: 50.0, height: 50.0)
+    {
+        didSet{
+            createButtons()
+        }
+    }
+    @IBInspectable var starsCount: Int = 5
+        {
+        didSet{
+            createButtons()
+        }
+    }
+    
+    //MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        createButtons()
+    }
+    
+    //MARK: Private methods
+    
+    private func createButtons()
+    {
+        for button in ratingButtons
+        {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starsCount
+        {
+            let button = UIButton()
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            button.backgroundColor = UIColor.blue
+        
+            button.addTarget(self, action: #selector(ratingControl.buttonRatingTapped(button:)), for: .touchUpInside)
+            
+            addArrangedSubview(button)
+            
+            ratingButtons.append(button)
+        }
+    }
+    
+    func buttonRatingTapped(button: UIButton)
+    {
+        print("botton pressed")
+    }
 }
