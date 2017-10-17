633982b5c07bcebfe3941689f4d3840769c55b8c
Robert Kuraj <kuraj.robert@gmail.com>
2017-10-17 12:45:52 +0200

    add navigation cancel action

--- foodTracker/foodTracker/MealViewController.swift
+++ foodTracker/foodTracker/MealViewController.swift
@@ -82,6 +82,9 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         meal = Meal(name: name, photo: photo, rating: rating)
     }
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
     
     //MARK: mealImageViewDelegate
     
